127bc0602c0baa9ecdaf8348eac2a6fac33c8109
J.Stone <jameson@stone.tc>
2020-09-13 19:44:05 -0400

    :contruction: dropping effort for now

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	prompt "github.com/c-bata/go-prompt"
 )
 
-//
-func completer(in prompt.Document) []prompt.Suggest {
+// promptMapping is a mapping of available conventional commits with emoji commits
+func promptMapping(in prompt.Document) []prompt.Suggest {
 	s := []prompt.Suggest{
 		{Text: "wip", Description: "work-in-progress 🚧 "},
 		{Text: "articles", Description: "Store the article text posted by user"},
@@ -18,7 +22,7 @@ func completer(in prompt.Document) []prompt.Suggest {
 }
 
 func main() {
-	in := prompt.Input("ccm>> ", completer,
+	in := prompt.Input("ccm>> ", promptMapping,
 		prompt.OptionTitle("CCM"),
 		prompt.OptionPrefixTextColor(prompt.Yellow),
 		prompt.OptionPreviewSuggestionTextColor(prompt.Blue),
@@ -26,6 +30,19 @@ func main() {
 		prompt.OptionSuggestionBGColor(prompt.DarkGray))
 
 	if in == "wip" {
-		fmt.Println("wip(issue-123): 🚧 ")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Issue: ")
+		issue, _ := reader.ReadString('\n')
+		fmt.Print("Your commit: ")
+		text, _ := reader.ReadString('\n')
+		commit := "\"wip(" + strings.TrimSpace(issue) + "): 🚧 " + strings.TrimSpace(text) + "\""
+		fmt.Println(commit)
+		stdout, err := exec.Command("git commit -m " + commit).Output()
+
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+		fmt.Print(string(stdout))
 	}
 }
